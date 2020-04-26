8e98ae03e18c06d0b6b56e0bee3f5fc9b07fe59e
Marcos Diniz Duarte <marcos.duarte@smarkio.com.br>
2020-04-26 15:35:25 -0300

    Testing error log

--- get.js
+++ get.js
@@ -8,8 +8,8 @@ export const main = handler(async (event, context) => {
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: event.pathParameters.id
+      userId: event.requestContext.identity.cognitoIdentityId
+      // noteId: event.pathParameters.id
     }
   };
 
